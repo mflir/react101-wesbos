ad68f497a85089d0b0ab3ebf946305f2a93f8e54
mflir <scavone.mf@gmail.com>
2019-04-07 21:09:06 -0300

    [WIP] add to order function

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -27,6 +27,15 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   };
 
+  addToOrder = key => {
+    //1. take a copy of the state
+    const order = { ...this.state.order };
+    //2. either add to the order, or update the number in out order
+    order[key] = order[key] + 1 || 1;
+    //3. call setState to update our spate object
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -34,11 +43,11 @@ class App extends React.Component {
           <Header tagline="Fresh SeaFood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-                <Fish key={key} details={this.state.fishes[key]}/>
+              <Fish key={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} index={key} />
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
